73a3d1b0f6a2a9d49a7a4e0374ba4591d7a0907c
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2022-02-15 14:28:07 +0000

    Adding in run process

--- application.py
+++ application.py
@@ -15,3 +15,10 @@ def info():
 @application.route("/reviews")
 def review():
     return render_template("reviews.html")
+
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
